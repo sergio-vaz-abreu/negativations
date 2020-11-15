faad09450a2079c86dff226c9ad1dce5deaab0a0
sergio <sergioabreu@ntopus.com.br>
2020-11-15 00:12:58 -0300

    # Implementando repositório de negativações da api legada
    * Buscando todas as negativações

---
+++ modules/negativation/infrastructure/negativation_legacy_repository.go
@@ -0,0 +1,75 @@
+package infrastructure
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/negativations/modules/negativation/domain"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type NegativationLegacyRepository interface {
+	GetAll() ([]*domain.Negativation, error)
+}
+
+var (
+	ReadNegativationError = errors.New("failed to read negativation from legacy system")
+)
+
+func NewNegativationLegacyRepositoryAPI(baseUrl string) (*NegativationLegacyRepositoryAPI, error) {
+	_, err := url.ParseRequestURI(baseUrl)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse legacy api base url")
+	}
+	return &NegativationLegacyRepositoryAPI{baseUrl: baseUrl}, nil
+}
+
+type NegativationLegacyRepositoryAPI struct {
+	baseUrl string
+}
+
+func (r *NegativationLegacyRepositoryAPI) GetAll() ([]*domain.Negativation, error) {
+	httpResponse, err := http.Get(fmt.Sprintf("%s/%s", r.baseUrl, "negativations"))
+	if err != nil {
+		logrus.
+			WithError(err).
+			Error("failed to send request to legacy api")
+		return nil, ReadNegativationError
+	}
+	body, err := ioutil.ReadAll(httpResponse.Body)
+	if body != nil {
+		defer httpResponse.Body.Close()
+	}
+	if err != nil {
+		logrus.
+			WithField("statusCode", httpResponse.StatusCode).
+			WithError(err).
+			Error("failed to read http response body")
+		return nil, ReadNegativationError
+	}
+	if !IsSuccess(httpResponse) {
+		logrus.
+			WithField("statusCode", httpResponse.StatusCode).
+			WithField("httpResponse", string(body)).
+			Error("failed to have a success status code")
+		return nil, ReadNegativationError
+	}
+	var negativations []*domain.Negativation
+	err = json.Unmarshal(body, &negativations)
+	if err != nil {
+		logrus.
+			WithField("statusCode", httpResponse.StatusCode).
+			WithField("httpResponse", string(body)).
+			WithError(err).
+			Error("failed to decode negativations")
+		return nil, ReadNegativationError
+	}
+	return negativations, nil
+}
+
+func IsSuccess(httpResponse *http.Response) bool {
+	return httpResponse.StatusCode >= 200 && httpResponse.StatusCode < 300
+}
---
+++ modules/negativation/infrastructure/negativation_legacy_repository_test.go
@@ -0,0 +1,59 @@
+package infrastructure
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestNegativationLegacyRepositoryAPI(t *testing.T) {
+	handler := gin.Default()
+	handler.GET("negativations", func(context *gin.Context) {
+		data := `
+[
+  {
+    "companyDocument":"59291534000167",
+    "companyName":"ABC S.A.",
+    "customerDocument":"51537476467",
+    "value":1235.23,
+    "contract":"bc063153-fb9e-4334-9a6c-0d069a42065b",
+    "debtDate":"2015-11-13T23:32:51Z",
+    "inclusionDate":"2020-11-13T23:32:51Z"
+  }
+]
+`
+		context.Writer.WriteHeader(http.StatusOK)
+		_, _ = context.Writer.WriteString(data)
+	})
+	server := httptest.NewServer(handler)
+	baseUrl := fmt.Sprintf("http://%s", server.Listener.Addr())
+
+	t.Run("Getting all negativations from legacy api", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		sut, err := NewNegativationLegacyRepositoryAPI(baseUrl)
+		g.Expect(err).Should(
+			Not(HaveOccurred()))
+
+		maintenances, err := sut.GetAll()
+
+		g.Expect(err).Should(
+			Not(HaveOccurred()))
+		g.Expect(maintenances).Should(
+			And(
+				HaveLen(1),
+				ContainElement(PointTo(MatchAllFields(Fields{
+					"CompanyDocument":  BeEquivalentTo("59291534000167"),
+					"CompanyName":      BeEquivalentTo("ABC S.A."),
+					"CustomerDocument": BeEquivalentTo("51537476467"),
+					"Value":            BeEquivalentTo(1235.23),
+					"Contract":         BeEquivalentTo("bc063153-fb9e-4334-9a6c-0d069a42065b"),
+					"DebtDate":         BeEquivalentTo(time.Date(2015, 11, 13, 23, 32, 51, 0, time.UTC)),
+					"InclusionDate":    BeEquivalentTo(time.Date(2020, 11, 13, 23, 32, 51, 0, time.UTC)),
+				})))))
+	})
+}
